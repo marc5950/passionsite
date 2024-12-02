f2fd52c422debe1cb2bc633b9233836db16dd033
marc5950 <marcuswagner5950@gmail.com>
2024-12-02 14:01:49 +0100

    comment scripts

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Select elements
-const burgerMenu = document.getElementById("burger-menu");
-const menu = document.getElementById("menu");
+// const burgerMenu = document.getElementById("burger-menu");
+// const menu = document.getElementById("menu");
 
-// Toggle the menu when the burger menu is clicked
-burgerMenu.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
+// burgerMenu.addEventListener("click", () => {
+//   menu.classList.toggle("active");
+// });
